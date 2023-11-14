cd9169fe5b004c89ab9011a86fa8f6ee7869d41a
Aluon <paulacanoroman@gmail.com>
2023-11-14 13:47:44 +0100

    desestructurar obj

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-
+// ----------EJERCICIO 1-----------------------
 const numbers = [1, 2, 3, 4, 5];
 
 const squaredNumbers = numbers.map((num)=>{
@@ -7,5 +7,18 @@ const squaredNumbers = numbers.map((num)=>{
     console.log(squared)
     // alert(squared)
 })
-
 squaredNumbers
+
+// ----------EJERCICIO 2-----------------------
+const students = [
+    { name: 'Alice', grade: 85 },
+    { name: 'Bob', grade: 90 },
+    { name: 'Charlie', grade: 78 },
+    { name: 'David', grade: 95 }
+  ];
+
+const highGrade = () =>{
+    const grade = students.map((obj)=>{console.log(obj.grade)})
+}
+
+highGrade()
